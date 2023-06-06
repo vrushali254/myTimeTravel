a93df5a16a041e125d0fb907a30ef01d6e2a517a
vrushali254 <vrushali254@gmail.com>
2023-06-06 01:10:24 -0700

    WIP wiring sqlite with springboot

--- RecordSystem/src/main/java/org/records/Server.java
+++ RecordSystem/src/main/java/org/records/Server.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Server {
+
     public static void main(String[] args) {
         System.out.println("Hello humans!");
         SpringApplication.run(Server.class, args);
